1c6bcc9162f122681aeaab2f7f2d9e563008c796
JeffWScott <jeffscott@outlook.com>
2021-11-17 16:22:10 -0500

    down for maintenance

--- src/App.svelte
+++ src/App.svelte
@@ -1,41 +1,44 @@
-<script lang="ts">
-import { Router, Route } from "svelte-routing";
+<script>
+	import { Router, Route } from "svelte-routing";
 
-// Components
-import Home from "./pages/Home.svelte";
-import MakeSwap from "./pages/MakeSwap.svelte";
-import Results from "./pages/Results.svelte";
-import Finish from "./pages/Finish.svelte";
-import PasteDetails from "./pages/PasteDetails.svelte";
-import Restart from "./pages/Restart.svelte";
-import Banner from "./components/Banner.svelte";
-import NavBar from "./components/Nav/NavigationBar.svelte";
-import Footer from "./components/Footer.svelte";
+	// Components
+	import Maintenance from "./pages/Maintenance.svelte";
+	import Home from "./pages/Home.svelte";
+	import MakeSwap from "./pages/MakeSwap.svelte";
+	import Results from "./pages/Results.svelte";
+	import Finish from "./pages/Finish.svelte";
+	import PasteDetails from "./pages/PasteDetails.svelte";
+	import Restart from "./pages/Restart.svelte";
+	import Banner from "./components/Banner.svelte";
+	import NavBar from "./components/Nav/NavigationBar.svelte";
+	import Footer from "./components/Footer.svelte";
 
 
-// Misc
-import { hydrateSwapHistory, hydrateSwapInfo } from './js/localstorage-utils'
+	// Misc
+	import { hydrateSwapHistory, hydrateSwapInfo } from './js/localstorage-utils'
 
-import { onMount } from 'svelte'
+	import { onMount } from 'svelte'
 
-onMount(() => {
-	hydrateSwapHistory()
-	hydrateSwapInfo()
-	unregisterOldServiceWorkers()
-})
+	let maintenance_on = true
 
-function unregisterOldServiceWorkers(){
-	if (typeof navigator === "undefined") return
-	if (typeof navigator.serviceWorker === "undefined") return
-
-	navigator.serviceWorker.getRegistrations()
-	.then(registrations => {
-		for(let registration of registrations) { 
-			registration.unregister(); 
-		}
-		if (registrations.length > 0) window.location.reload()
+	onMount(() => {
+		hydrateSwapHistory()
+		hydrateSwapInfo()
+		unregisterOldServiceWorkers()
 	})
-}
+
+	function unregisterOldServiceWorkers(){
+		if (typeof navigator === "undefined") return
+		if (typeof navigator.serviceWorker === "undefined") return
+
+		navigator.serviceWorker.getRegistrations()
+		.then(registrations => {
+			for(let registration of registrations) { 
+				registration.unregister(); 
+			}
+			if (registrations.length > 0) window.location.reload()
+		})
+	}
 </script>
 
 <style>
@@ -65,18 +68,22 @@ function unregisterOldServiceWorkers(){
 </style>
 
 <Router>
-<Banner />
-<NavBar />
-<main>
-	<Route path="/" component={Home} />
-	<Route path="/swap" component={MakeSwap} />
-	<Route path="/swap/:testnet" component={MakeSwap} />
-	<Route path="/results" component={Results} />
-	<Route path="/finish" component={Finish} />
-	<Route path="/pastedetails" component={PasteDetails} />
-	<Route path="/restart" component={Restart} />
-</main>
-<Footer />
+	<Banner />
+	<NavBar />
+	<main>
+		{#if maintenance_on}
+			<Route path="/" component={Maintenance} />
+		{:else}
+			<Route path="/" component={Home} />
+			<Route path="/swap" component={MakeSwap} />
+			<Route path="/swap/:testnet" component={MakeSwap} />
+			<Route path="/results" component={Results} />
+			<Route path="/finish" component={Finish} />
+			<Route path="/pastedetails" component={PasteDetails} />
+			<Route path="/restart" component={Restart} />
+		{/if}
+	</main>
+	<Footer />
 </Router>
 
 
---
+++ src/pages/Maintenance.svelte
@@ -0,0 +1,25 @@
+<style>
+	.subtitle{
+		font-size: 1em;
+	}
+	p{
+		margin: 0.5rem 0.5em 0 0;
+		color: var(--font-primary-dim);	
+		width: max-content;
+	}
+
+	.help-link{
+		margin-top: 0.5rem;
+		font-weight: 100;
+		align-self: flex-end;
+	}
+</style>
+
+<h2>We'll be back soon...</h2>
+<div class="subtitle flex row wrap align-center">
+	<p class="subtitle">Lamden Link is down for maintenance. </p>
+	<a class="help-link" href="https://t.me/lamdenlinksupport" target="_blank" rel="noopener noreferrer" > check for updates </a>
+</div>
+
+
+
