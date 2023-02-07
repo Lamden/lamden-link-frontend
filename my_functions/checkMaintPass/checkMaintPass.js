6e9a918e7d2e512d1cdf3c9228d9475a31ad3ede
JeffWScott <jeffscott@outlook.com>
2023-02-06 20:59:42 -0500

    remove logs

--- my_functions/checkMaintPass/checkMaintPass.js
+++ my_functions/checkMaintPass/checkMaintPass.js
@@ -1,7 +1,6 @@
 exports.handler = async (event) => {
 	const { password } = event.queryStringParameters
-	console.log(process.env.MAINT_PASS)
-	console.log(process.env)
+
 	if (password === process.env.MAINT_PASS){
 		return {
 			statusCode: 200,
